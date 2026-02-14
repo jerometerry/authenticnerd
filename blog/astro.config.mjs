d58fc4aeea36096d0f5fb1a6991d19221297cc01
Jerome Terry <jerome.l.terry@gmail.com>
2026-02-14 17:41:38 -0330

    Added robots.txt for SEO

--- blog/astro.config.mjs
+++ blog/astro.config.mjs
@@ -4,6 +4,7 @@ import sitemap from '@astrojs/sitemap';
 import preact from "@astrojs/preact";
 
 export default defineConfig({
+  site: "https://authenticnerd.com",
   integrations: [sitemap(), preact(
     {
       compat: true,
