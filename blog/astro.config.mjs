dce0fe34cf5cde68c06872fa94cdc5817b12e85f
Jerome Terry <jerome.l.terry@gmail.com>
2026-02-14 17:34:50 -0330

    Adding sitemap for SEO

--- blog/astro.config.mjs
+++ blog/astro.config.mjs
@@ -1,9 +1,10 @@
 import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
 
 import preact from "@astrojs/preact";
 
 export default defineConfig({
-  integrations: [preact(
+  integrations: [sitemap(), preact(
     {
       compat: true,
     }
