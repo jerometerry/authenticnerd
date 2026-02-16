34cae3d7e529874b405c4347be80866b0098748e
Jerome Terry <jerome.l.terry@gmail.com>
2026-02-16 15:41:46 -0330

    Starting converting to TailwindCSS

--- blog/astro.config.mjs
+++ blog/astro.config.mjs
@@ -3,14 +3,22 @@ import sitemap from '@astrojs/sitemap';
 
 import preact from "@astrojs/preact";
 
+import tailwindcss from '@tailwindcss/vite';
+
 export default defineConfig({
   site: "https://authenticnerd.com",
+
   build: {
     inlineStylesheets: 'always'
   },
+
   integrations: [sitemap(), preact(
     {
       compat: true,
     }
   )],
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
