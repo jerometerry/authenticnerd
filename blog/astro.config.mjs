169b0c8375a9faccbf4987a4eb16ca9ffca44c77
Jerome Terry <jerome.l.terry@gmail.com>
2026-02-16 23:12:47 -0330

    removing whiespace

--- blog/astro.config.mjs
+++ blog/astro.config.mjs
@@ -1,29 +1,22 @@
 import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
-
 import preact from "@astrojs/preact";
-
 import tailwindcss from '@tailwindcss/vite';
-
 import mdx from '@astrojs/mdx';
 
 export default defineConfig({
   site: "https://authenticnerd.com",
-
   build: {
     inlineStylesheets: 'always'
   },
-
   integrations: [sitemap(), preact(
     {
       compat: true,
     }
   ), mdx()],
-
   vite: {
     plugins: [tailwindcss()],
   },
-
   markdown: {
     shikiConfig: {
       theme: 'github-dark',
